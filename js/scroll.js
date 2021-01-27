41a98134cce6123674932abdf003dac6a064f09c
Arafat563 <53280293+Arafat563@users.noreply.github.com>
2021-01-27 13:34:46 +0100

    Add files via upload

---
+++ js/scroll.js
@@ -0,0 +1,23 @@
+<script>
+//get the button
+mybutton = document.getElementById("myBtn");
+
+// when the user scrolls down 20px from the top of the document,
+ // show the button
+ window.onscroll = function() {scrollFunction()};
+
+ function scrollFunction(){
+   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+       mybutton.style.display = "block";
+     }else {
+       mybutton.style.display = "none";
+     }
+ }
+
+ // when the user clicks on the button,
+ // scroll to the top of the document
+function topFunction(){
+  document.body.scrollTop = 0; //for Safari
+  document.documentElement.scrollTop = 0; //for Chrome, Firefox, IE and Opera
+}
+</script>
